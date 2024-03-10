e6d5b597564c0bca604fc7eacae2bb1e6b26b522
flamonrose23 <fatinemar20@icloud.com>
2024-03-10 10:55:03 -0700

    Test

---
+++ models/basemodel.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Base Model for AirBnB
+"""
+from datetime import datetime
+import json
+from uuid import uuid4
+import models
+
+
+class BaseModel:
+    """Class that defines all common attributes""" \
+        """and methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of the class"""
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
+
+    def __str__(self):
+        """String representation of the object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the instance with the current datetime and saves it"""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing all the """\
+            """keys and values of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
